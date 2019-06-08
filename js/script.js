ce2d72856c4c501d4b3f9cf4b836c8ef72d0a982
Kinkandju <ferretsphilosophy@gmail.com>
2019-06-08 09:41:52 +0300

    Добавление скриптов (пока не рабочих) + обратная стилизация вложенного списка (потому что так прям идеально по пикселям)

---
+++ js/script.js
@@ -0,0 +1,58 @@
+  var link = document.querySelector(".modal-button");
+
+  var popup = document.querySelector(".modal-write-us");
+  var close = popup.querySelector(".modal-close");
+
+  var form = popup.querySelector("form");
+  var user = popup.querySelector(".user-name-modal");
+  var email = popup.querySelector(".email-modal");
+
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("user");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  link.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
+
+      if (storage) {
+      user.value = storage;
+      email.focus();
+    } else {
+      user.focus();
+    }
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!user.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("user", user.value);
+      }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
