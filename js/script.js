2e3e6fe03594471d4a3afc79d09be48872577a98
Kinkandju <ferretsphilosophy@gmail.com>
2019-06-16 18:16:41 +0300

    Подключение анимации через работу js

--- js/script.js
+++ js/script.js
@@ -48,11 +48,36 @@
   });
 
   window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+    if (evt.keyCode === 27)
+    {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
         popup.classList.remove("modal-error");
+      }
+    }
+  });
+
+  var mapLink = document.querySelector(".map");
+
+  var mapPopup = document.querySelector(".modal-map");
+  var mapClose = mapPopup.querySelector(".modal-close");
+
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
       }
     }
   });
